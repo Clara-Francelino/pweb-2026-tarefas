7a0796dba6f65a87b0de0efffd2ae9e5e42798a0
Clara-Francelino <clarafrancelinoaaa@gmail.com>
2026-06-23 20:31:43 -0300

    tarefa 04 pronta

---
+++ tarefa04/tarefas/views.py
@@ -0,0 +1,12 @@
+from datetime import date 
+from django.shortcuts import render
+from .models import Tarefa
+
+def lista_tarefas(request):
+    tarefas = Tarefa.objects.all()
+    
+    context = {
+        'tarefas': tarefas,
+        'hoje': date.today()
+    }
+    return render(request, 'tarefas/lista_tarefas.html', context)
